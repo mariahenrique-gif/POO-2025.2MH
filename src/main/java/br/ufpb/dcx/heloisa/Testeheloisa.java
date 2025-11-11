3117edd57ba9b77807fe3f6dc1c58bca2c5e1ed3
maria.henrique <maria.henrique@dcx.ufpb.br>
2025-11-11 09:18:33 -0300

    Criando nova classe de teste utilizando IO

---
+++ src/main/java/br/ufpb/dcx/heloisa/Testeheloisa.java
@@ -0,0 +1,9 @@
+package br.ufpb.dcx.heloisa;
+
+public class Testeheloisa {
+    void main(){
+        IO.println("Oi ");
+        String nome = IO.readln("Digite seu nome");
+        IO.println("Oi " + nome);
+    }
+}
