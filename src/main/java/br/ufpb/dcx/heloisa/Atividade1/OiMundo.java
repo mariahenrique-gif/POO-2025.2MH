b39f3743471c65455e1ac42b2f69be2df579a768
Maria Heloisa <maria.henrique@dcx.ufpb.br>
2025-12-15 00:30:12 -0300

    Atividade 1 de POO revisão com OiMundo, Aluno e TestaAluno

---
+++ src/main/java/br/ufpb/dcx/heloisa/Atividade1/OiMundo.java
@@ -0,0 +1,7 @@
+package br.ufpb.dcx.heloisa.Atividade1;
+
+public class OiMundo {
+    public static void main (String [] args){
+        System.out.print("Oi Mundo");
+    }
+}
---
+++ src/main/java/br/ufpb/dcx/heloisa/Atividade1/TestaAluno.java
@@ -0,0 +1,28 @@
+package br.ufpb.dcx.heloisa.Atividade1;
+
+import javax.swing.*;
+
+public class TestaAluno {
+    public static void main(String[] args) {
+        int quantidadeAlunos = 3;
+        Aluno[] alunos = new Aluno[quantidadeAlunos];
+
+        for (int k = 0; k < quantidadeAlunos; k++) {
+            String nomeAluno = JOptionPane.showInputDialog("Nome do Aluno:");
+            String matriculaAluno = JOptionPane.showInputDialog("Matrícula do Aluno:");
+
+            double[] notas = new double[3];
+            for (int i = 0; i < 3; i++) {
+                String notaStr = JOptionPane.showInputDialog("Nota " + (i + 1) + " do Aluno:");
+                notas[i] = Double.parseDouble(notaStr);
+            }
+
+            alunos[k] = new Aluno(nomeAluno, matriculaAluno, notas);
+        }
+
+        System.out.println("Dados dos alunos:");
+        for (Aluno a : alunos) {
+            System.out.println(a);
+        }
+    }
+}
